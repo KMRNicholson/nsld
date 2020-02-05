c0a45ab4d1ddfc6156f2dec47e42fc293e5885b8
kmrnicholson <kohdy.nicholson@gmail.com>
2020-02-05 21:15:47 +0100

    Code for Database constructor

---
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/DAL/Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using NeverSkipLegDay.Models;
+
+namespace NeverSkipLegDay.DAL
+{
+    class Database
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public Database(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Workout>().Wait();
+        }
+    }
+}
