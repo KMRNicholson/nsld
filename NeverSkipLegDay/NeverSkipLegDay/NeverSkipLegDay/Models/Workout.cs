3513ba21281362cdba64732b61a31abf5d66223e
Kohdy Nicholson <KNI@milestone.dk>
2020-02-06 15:48:25 +0100

    removed some abstraction as I am planning to keep it simple and stupid for now.

--- NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Models/Workout.cs
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Models/Workout.cs
@@ -5,9 +5,11 @@ using SQLite;
 
 namespace NeverSkipLegDay.Models
 {
-    class Workout : Model
+    class Workout
     {
         [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
         public string Name { get; set; }
+        public DateTime Date { get; set; }
     }
 }
