7370d349e2d9d0f6a2b0dded0f2db20fc619cc7f
kmrnicholson <kohdy.nicholson@gmail.com>
2020-02-12 18:35:28 +0100

    Tests not working.

--- NeverSkipLegDay/NeverSkipLegDay.UnitTests/WorkoutDALTest.cs
+++ NeverSkipLegDay/NeverSkipLegDay.UnitTests/WorkoutDALTest.cs
@@ -9,20 +9,40 @@ namespace NeverSkipLegDay.UnitTests
 {
     [TestClass]
     public class WorkoutDALTest
-    {
-        static WorkoutDAL workoutDAL = new WorkoutDAL(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "workouts.db3"));
-        
+    {        
         [TestMethod]
-        public void SaveNoteAsyncTest()
+        public void InsertNoteAsyncTest()
         {
+            WorkoutDAL workoutDAL = new WorkoutDAL(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "workouts.db3"));
             Workout workout = new Workout();
             workout.ID = 1;
             workout.Name = "Push Day";
             workout.Date = new DateTime().ToUniversalTime();
-            Task<int> result = workoutDAL.SaveNoteAsync(workout);
-            result.Wait();
+            Task<int> saveTask = workoutDAL.SaveNoteAsync(workout);
+            saveTask.Wait();
 
-            Assert.AreEqual(result.Result, 0);
+            Assert.AreEqual(saveTask.Result, 0);
+        }
+
+        [TestMethod]
+        public void UpdateNoteAsyncTest()
+        {
+            WorkoutDAL workoutDAL = new WorkoutDAL(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "workouts.db3"));
+            Workout workout = new Workout();
+            workout.ID = 1;
+            workout.Name = "Pull Day";
+            workout.Date = new DateTime().ToUniversalTime();
+            Task<int> saveTask = workoutDAL.SaveNoteAsync(workout);
+            saveTask.Wait();
+
+            Assert.AreEqual(saveTask.Result, 0);
+
+            Task<Workout> readTask = workoutDAL.GetNoteAsync(workout.ID);
+            readTask.Wait();
+
+            Workout updatedWorkout = readTask.Result;
+
+            Assert.AreEqual(updatedWorkout.Name, workout.Name);
         }
     }
 }
