94f25eac6e47620cbf1dbda8b836d8ef3218ece7
kmrnicholson <kohdy.nicholson@gmail.com>
2020-02-12 18:21:14 +0100

    Test for Saving a note

--- NeverSkipLegDay/NeverSkipLegDay.UnitTests/WorkoutDALTest.cs
+++ NeverSkipLegDay/NeverSkipLegDay.UnitTests/WorkoutDALTest.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NeverSkipLegDay.DAL;
+using NeverSkipLegDay.Models;
 
 namespace NeverSkipLegDay.UnitTests
 {
@@ -9,10 +11,18 @@ namespace NeverSkipLegDay.UnitTests
     public class WorkoutDALTest
     {
         static WorkoutDAL workoutDAL = new WorkoutDAL(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "workouts.db3"));
+        
         [TestMethod]
         public void SaveNoteAsyncTest()
         {
-            
+            Workout workout = new Workout();
+            workout.ID = 1;
+            workout.Name = "Push Day";
+            workout.Date = new DateTime().ToUniversalTime();
+            Task<int> result = workoutDAL.SaveNoteAsync(workout);
+            result.Wait();
+
+            Assert.AreEqual(result.Result, 0);
         }
     }
 }
