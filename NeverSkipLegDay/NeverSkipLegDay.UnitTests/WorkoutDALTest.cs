8db1150040425b20b510e5098fb7c96fc29aebf9
kmrnicholson <kohdy.nicholson@gmail.com>
2020-02-12 17:56:47 +0100

    WorkoutDAL constructor test.

---
+++ NeverSkipLegDay/NeverSkipLegDay.UnitTests/WorkoutDALTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeverSkipLegDay.DAL;
+
+namespace NeverSkipLegDay.UnitTests
+{
+    [TestClass]
+    public class WorkoutDALTest
+    {
+        static WorkoutDAL workoutDAL;
+        [TestMethod]
+        public void TestConstructor()
+        {
+            workoutDAL = new WorkoutDAL(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "workouts.db3"));
+        }
+    }
+}
