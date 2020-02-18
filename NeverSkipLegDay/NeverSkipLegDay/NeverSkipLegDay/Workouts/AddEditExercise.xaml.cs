50120c7f80d864feda46464dc1be894b089b45a3
kmrnicholson <kohdy.nicholson@gmail.com>
2020-02-18 20:54:49 +0100

    Sets are now created on exercise creation

--- NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Workouts/AddEditExercise.xaml.cs
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Workouts/AddEditExercise.xaml.cs
@@ -24,6 +24,12 @@ namespace NeverSkipLegDay.Workouts
             var exercise = (Models.Exercise)BindingContext;
             exercise.Date = DateTime.UtcNow;
             await App.ExerciseDAL.SaveExerciseAsync(exercise);
+            for (int i = 0; i < exercise.Sets; i++)
+            {
+                Set set = new Set();
+                set.ExerciseID = exercise.ID;
+                await App.SetDAL.SaveSetAsync(set);
+            }
             await Navigation.PopAsync();
         }
     }
