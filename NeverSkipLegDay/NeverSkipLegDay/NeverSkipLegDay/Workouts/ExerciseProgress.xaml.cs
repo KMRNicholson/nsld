f85539c4fec4ecdb845aa7118c87d0cfe83786d3
kmrnicholson <kohdy.nicholson@gmail.com>
2020-02-18 22:14:45 +0100

    The layout for editing progress is in place.

--- NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Workouts/ExerciseProgress.xaml.cs
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Workouts/ExerciseProgress.xaml.cs
@@ -16,5 +16,31 @@ namespace NeverSkipLegDay.Workouts
         {
             InitializeComponent();
         }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            Models.Exercise exercise = (Models.Exercise)this.BindingContext;
+            listView.ItemsSource = await App.SetDAL.GetSetsByExerciseIdAsync(exercise.ID);
+        }
+
+        async void OnSave(object sender, EventArgs e)
+        {
+            //var exercise = (Models.Exercise)BindingContext;
+            //exercise.Date = DateTime.UtcNow;
+            //await App.ExerciseDAL.SaveExerciseAsync(exercise);
+            //for (int i = 0; i < exercise.Sets; i++)
+            //{
+            //    Set set = new Set();
+            //    set.ExerciseID = exercise.ID;
+            //    await App.SetDAL.SaveSetAsync(set);
+            //}
+            //await Navigation.PopAsync();
+        }
+
+        private void CancelSelection(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null) return;
+            ((ListView)sender).SelectedItem = null;
+        }
     }
 }
