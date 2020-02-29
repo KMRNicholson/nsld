c8734d7fa85dabbec3a77c39f6c44c794cf87984
kmrnicholson <kohdy.nicholson@gmail.com>
2020-02-29 17:07:45 +0100

    SetsPage code-behind files is fixed up.

--- NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Views/Workouts/SetsPage.xaml.cs
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Views/Workouts/SetsPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NeverSkipLegDay.Models.DAL;
+using NeverSkipLegDay.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,49 +14,22 @@ namespace NeverSkipLegDay.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SetsPage : ContentPage
     {
-        static int render = 0;
-        public SetsPage()
+        public SetsPageViewModel ViewModel
         {
+            get { return BindingContext as SetsPageViewModel; }
+            set { BindingContext = value; }
+        }
+        public SetsPage(ExerciseViewModel exercise)
+        {
+            var setDal = new SetDal(new SQLiteDB());
+            var pageService = new PageService();
+            ViewModel = new SetsPageViewModel(exercise, setDal, pageService);
             InitializeComponent();
         }
-        protected override async void OnAppearing()
+        protected override void OnAppearing()
         {
+            ViewModel.LoadDataCommand.Execute(null);
             base.OnAppearing();
-            Models.Exercise exercise = (Models.Exercise)this.BindingContext;
-            render = 0;
-            listView.ItemsSource = await App.SetDAL.GetSetsByExerciseIdAsync(exercise.ID);
-        }
-
-        async void OnSave(object sender, EventArgs e)
-        {
-            Button save = (Button)sender;
-            Models.Set set = (Models.Set)save.BindingContext;
-            set.Date = DateTime.UtcNow;
-            await App.SetDAL.SaveSetAsync(set);
-            save.IsEnabled = false;
-            save.Opacity = 0;
-        }
-
-        private void CancelSelection(object sender, SelectedItemChangedEventArgs e)
-        {
-            if (e.SelectedItem == null) return;
-            ((ListView)sender).SelectedItem = null;
-        }
-
-        void OnEdit(object sender, EventArgs e)
-        {
-            Models.Exercise exercise = (Models.Exercise)BindingContext;
-            Entry entry = (Entry)sender;
-            StackLayout stackLayout = (StackLayout)entry.Parent;
-            Button save = (Button)stackLayout.Children.Last();
-
-            if (render > ((exercise.Sets * 2))-1)
-            {
-                save.IsEnabled = true;
-                save.Opacity = 1;
-            }
-
-            render++;
         }
     }
 }
