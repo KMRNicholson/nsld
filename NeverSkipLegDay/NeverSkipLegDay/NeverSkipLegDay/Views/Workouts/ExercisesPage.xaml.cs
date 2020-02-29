5e29976fa17e5fdc1e7e84e4c47e1dd4a948ddfe
kmrnicholson <kohdy.nicholson@gmail.com>
2020-02-29 15:43:29 +0100

    ExercisesPage code-behind file fixed.

--- NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Views/Workouts/ExercisesPage.xaml.cs
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Views/Workouts/ExercisesPage.xaml.cs
@@ -1,78 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
+using NeverSkipLegDay.ViewModels;
+using NeverSkipLegDay.Models.DAL;
 
 namespace NeverSkipLegDay.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ExercisesPage : ContentPage
     {
+        public ExercisesPageViewModel ViewModel
+        {
+            get { return BindingContext as ExercisesPageViewModel; }
+            set { BindingContext = value; }
+        }
         public ExercisesPage()
         {
+            var exerciseDal = new ExerciseDal(new SQLiteDB());
+            var pageService = new PageService();
+            ViewModel = new ExercisesPageViewModel(exerciseDal, pageService);
             InitializeComponent();
         }
 
-        protected override async void OnAppearing()
+        protected override void OnAppearing()
         {
+            ViewModel.LoadDataCommand.Execute(null);
             base.OnAppearing();
-            Models.Workout workout = (Models.Workout)this.BindingContext;
-            listView.ItemsSource = await App.ExerciseDAL.GetExercisesByWorkoutIdAsync(workout.ID);
-            List<Models.Exercise> exercises = (List<Models.Exercise>)listView.ItemsSource;
-            helpLabel.IsVisible = exercises.Count == 0 ? true : false;
         }
 
-        async void OnAddOrEdit(object sender, EventArgs e)
+        void OnExerciseSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            Models.Exercise exercise;
-            string[] args = ((Button)sender).BindingContext.ToString().Split(',');
-            string operation = args[0];
-            int id = Int32.Parse(args[1]);
-            if(operation == "Add")
-            {
-                exercise = new Models.Exercise
-                {
-                    WorkoutID = id
-                };
-
-                await Navigation.PushAsync(new AddEditExercisePage
-                {
-                    BindingContext = exercise as Models.Exercise
-                });
-            }
-            else
-            {
-                exercise = await App.ExerciseDAL.GetExerciseAsync(id);
-
-                await Navigation.PushAsync(new EditExercise
-                {
-                    BindingContext = exercise as Models.Exercise
-                });
-            }
-        }
-        async void OnDelete(object sender, EventArgs e)
-        {
-            var id = ((Button)sender).BindingContext;
-            Models.Exercise exercise = await App.ExerciseDAL.GetExerciseAsync((int)id);
-            await App.ExerciseDAL.DeleteExerciseAsync(exercise);
-            listView.ItemsSource = await App.ExerciseDAL.GetExercisesByWorkoutIdAsync((int)id);
-            List<Models.Exercise> exercises = (List<Models.Exercise>)listView.ItemsSource;
-            helpLabel.IsVisible = exercises.Count == 0 ? true : false;
-        }
-
-        async void OnExerciseSelected(object sender, SelectedItemChangedEventArgs e)
-        {
-            if (e.SelectedItem != null)
-            {
-                await Navigation.PushAsync(new ExerciseProgress
-                {
-                    BindingContext = e.SelectedItem as Models.Exercise
-                });
-            }
+            ViewModel.SelectExerciseCommand.Execute(e.SelectedItem);
         }
     }
 }
