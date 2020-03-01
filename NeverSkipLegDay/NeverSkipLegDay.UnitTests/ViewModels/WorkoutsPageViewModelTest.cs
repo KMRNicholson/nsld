4e4ae984d705a93bf01c8d0871eaaf0b0c145ba2
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-01 15:22:07 +0100

    added moq to the Nuget packages

---
+++ NeverSkipLegDay/NeverSkipLegDay.UnitTests/ViewModels/WorkoutsPageViewModelTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NeverSkipLegDay.UnitTests.ViewModels
+{
+    [TestClass]
+    public class WorkoutsPageViewModelTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
