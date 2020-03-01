18d919bac1f8b733695e2f9062837b2f9cab5744
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-01 16:11:29 +0100

    Checking in NUnit teset project so that I can run it with dotnet test

---
+++ NeverSkipLegDay/NeverSkipLegDay.NUnitTestProject/ViewModels/WorkoutViewModelTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using System.Text;
+using System.Threading.Tasks;
+using NeverSkipLegDay.Models;
+using NeverSkipLegDay.UnitTests.Database;
+using NeverSkipLegDay.ViewModels;
+
+namespace NeverSkipLegDay.UnitTests.ViewModels
+{
+    [TestFixture]
+    public class WorkoutViewModelTest
+    {
+        private WorkoutDatabase _db = new WorkoutDatabase();
+        
+        [Test]
+        public void ConstructorTest()
+        {
+            Workout workout = _db.GetWorkouts().FirstOrDefault();
+            WorkoutViewModel workoutViewModel = new WorkoutViewModel(workout);
+
+            Assert.AreEqual(workoutViewModel.Id, workout.Id);
+            Assert.AreEqual(workoutViewModel.Name, workout.Name);
+        }
+
+        [Test]
+        public void OnPropertyChangedTest()
+        {
+            Workout workout = _db.GetWorkouts().FirstOrDefault();
+            WorkoutViewModel workoutViewModel = new WorkoutViewModel(workout);
+
+            string newName = "New Workout";
+
+            workoutViewModel.Name = newName;
+
+            Assert.AreNotEqual(workoutViewModel.Name, workout.Name);
+            Assert.AreEqual(workoutViewModel.Name, newName);
+        }
+    }
+}
