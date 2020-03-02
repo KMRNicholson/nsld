f2c39db3890334fb2ab3efcad791d3ba61c7dab1
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-02 14:57:18 +0100

    ExerciseViewModelTest are complete. :D

--- NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay.NUnitTestProject/ViewModels/ExerciseViewModelTest.cs
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay.NUnitTestProject/ViewModels/ExerciseViewModelTest.cs
@@ -9,5 +9,35 @@ namespace NeverSkipLegDay.NUnitTestProject.ViewModels
     [TestFixture]
     public class ExerciseViewModelTest
     {
+        private ExerciseDatabase mockDatabase;
+        private ExerciseViewModel viewModel;
+        private Exercise Exercise;
+
+        [SetUp]
+        public void Constructor()
+        {
+            mockDatabase = new ExerciseDatabase();
+            Exercise = mockDatabase.GetExercises().FirstOrDefault();
+            viewModel = new ExerciseViewModel(Exercise);
+        }
+
+        [Test]
+        public void ConstructorTest()
+        {
+            Assert.AreEqual(viewModel.Id, Exercise.Id);
+            Assert.AreEqual(viewModel.WorkoutId, Exercise.WorkoutId);
+            Assert.AreEqual(viewModel.Name, Exercise.Name);
+        }
+
+        [Test]
+        public void OnPropertyChangedTest()
+        {
+            string newName = "New Exercise";
+
+            viewModel.Name = newName;
+
+            Assert.AreNotEqual(viewModel.Name, Exercise.Name);
+            Assert.AreEqual(viewModel.Name, newName);
+        }
     }
 }
