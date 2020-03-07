e70c7003857c9538878b69985a3079f42f3f108d
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-07 17:34:08 +0100

    Interface for the meal dal

---
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Models/DAL/IMealDal.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace NeverSkipLegDay.Models.DAL
+{
+    public interface IMealDal
+    {
+        List<Meal> GetMeals();
+        Meal GetMeal(int id);
+        int SaveMeal(Meal model);
+        int DeleteMeal(Meal model);
+    }
+}
