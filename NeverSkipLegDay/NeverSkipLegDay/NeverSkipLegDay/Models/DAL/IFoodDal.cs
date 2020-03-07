fbd9e602735ec2e212623808dd555639ee3693e5
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-07 23:50:47 +0100

    FoodDal interface and class created.

---
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Models/DAL/IFoodDal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace NeverSkipLegDay.Models.DAL
+{
+    public interface IFoodDal
+    {
+        List<Food> GetFoods();
+        Food GetFood(int id);
+        List<Food> GetFoodsByMealId(int mealId);
+        int SaveFood(Food model);
+        int DeleteFood(Food model);
+    }
+}
