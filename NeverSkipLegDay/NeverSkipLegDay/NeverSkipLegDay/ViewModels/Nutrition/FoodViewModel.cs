d7528545648262ad235550366bc89b6e46f0a5fe
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-08 14:36:40 +0100

    Added view models for food

---
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/ViewModels/Nutrition/FoodViewModel.cs
@@ -0,0 +1,39 @@
+﻿using NeverSkipLegDay.Models;
+
+namespace NeverSkipLegDay.ViewModels
+{
+    public class FoodViewModel : BaseViewModel
+    {
+        public int Id { get; set; }
+        private int _mealId;
+        public int MealId
+        {
+            get { return _mealId; }
+            set
+            {
+                SetValue(ref _mealId, value);
+                OnPropertyChanged(nameof(MealId));
+            }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                SetValue(ref _name, value);
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public FoodViewModel() { }
+
+        public FoodViewModel(Food food)
+        {
+            Id = food.Id;
+            MealId = food.MealId;
+            Name = food.Name;
+        }
+    }
+}
---
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/ViewModels/Nutrition/FoodsPageViewModel.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using NeverSkipLegDay.Models;
+using NeverSkipLegDay.Models.DAL;
+using NeverSkipLegDay.Views;
+using Xamarin.Forms;
+
+namespace NeverSkipLegDay.ViewModels
+{
+    public class FoodsPageViewModel : BaseViewModel
+    {
+        private FoodViewModel _selectedFood;
+        private IFoodDal _foodDal;
+        private IPageService _pageService;
+
+        private bool _isDataLoaded;
+
+        public MealViewModel Meal { get; private set; }
+
+        public string AddButtonText
+        {
+            get { return "Add Food"; }
+        }
+
+        public ObservableCollection<FoodViewModel> Foods { get; private set; }
+            = new ObservableCollection<FoodViewModel>();
+
+        public FoodViewModel SelectedFood
+        {
+            get { return _selectedFood; }
+            set { SetValue(ref _selectedFood, value); }
+        }
+
+        public ICommand LoadDataCommand { get; set; }
+        public ICommand AddCommand { get; set; }
+        public ICommand EditCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+        public ICommand SelectCommand { get; set; }
+
+        public FoodsPageViewModel(MealViewModel meal, IFoodDal foodDal, IPageService pageService)
+        {
+            MessagingCenter.Subscribe<AddEditFoodPageViewModel, Food>
+                (this, Events.FoodSaved, OnFoodSaved);
+
+            _foodDal = foodDal;
+            _pageService = pageService;
+
+            Meal = meal;
+
+            LoadDataCommand = new Command(() => LoadData());
+            AddCommand = new Command(async () => await AddFood());
+            EditCommand = new Command<FoodViewModel>(async food => await EditFood(food));
+            DeleteCommand = new Command<FoodViewModel>(async food => await DeleteFood(food));
+            SelectCommand = new Command<FoodViewModel>(async food => await SelectFood(food));
+        }
+
+        private void OnFoodSaved(AddEditFoodPageViewModel source, Food food)
+        {
+            FoodViewModel foodInList = Foods.Where(e => e.Id == food.Id).ToList().FirstOrDefault();
+
+            if (foodInList == null)
+            {
+                Foods.Add(new FoodViewModel(food));
+            }
+            else
+            {
+                foodInList.Id = food.Id;
+                foodInList.Name = food.Name;
+            }
+        }
+
+        public void LoadData()
+        {
+            if (_isDataLoaded) return;
+
+            _isDataLoaded = true;
+            var foods = _foodDal.GetFoodsByMealId(Meal.Id);
+            foreach (var food in foods)
+            {
+                Foods.Add(new FoodViewModel(food));
+            }
+        }
+
+        private async Task AddFood()
+        {
+            await _pageService.PushAsync(new AddEditFoodPage(new FoodViewModel() { MealId = Meal.Id }));
+        }
+
+        private async Task EditFood(FoodViewModel food)
+        {
+            if (food == null) return;
+
+            await _pageService.PushAsync(new AddEditFoodPage(food));
+        }
+
+        public async Task DeleteFood(FoodViewModel food)
+        {
+            if (food == null) return;
+
+            if (await _pageService.DisplayAlert("Warning", $"Are you sure you want to delete {food.Name}?", "Yes", "No"))
+            {
+                var foodModel = _foodDal.GetFood(food.Id);
+                Foods.Remove(food);
+                _foodDal.DeleteFood(foodModel);
+            }
+        }
+
+        private async Task SelectFood(FoodViewModel food)
+        {
+            if (food == null) return;
+
+            SelectedFood = null;
+            await _pageService.PushAsync(new SetsPage(food));
+        }
+
+        public bool IsFoodsEmpty()
+        {
+            return Foods.Count == 0 ? true : false;
+        }
+    }
+}
