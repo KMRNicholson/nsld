c18feef16c23c9e031e03e65e0239115eb19eb65
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-08 14:41:59 +0100

    Views are created for foods

--- NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Views/Nutrition/FoodsPage.xaml.cs
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Views/Nutrition/FoodsPage.xaml.cs
@@ -1,21 +1,37 @@
-﻿using NeverSkipLegDay.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
+using NeverSkipLegDay.ViewModels;
+using NeverSkipLegDay.Models.DAL;
 
 namespace NeverSkipLegDay.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FoodsPage : ContentPage
     {
+        public FoodsPageViewModel ViewModel
+        {
+            get { return BindingContext as FoodsPageViewModel; }
+            set { BindingContext = value; }
+        }
         public FoodsPage(MealViewModel meal)
         {
+            var foodDal = new FoodDal(new SQLiteDB());
+            var pageService = new PageService();
+            ViewModel = new FoodsPageViewModel(meal, foodDal, pageService);
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ViewModel.LoadDataCommand.Execute(null);
+            helpLabel.IsVisible = ViewModel.IsFoodsEmpty();
+        }
+
+        void OnFoodSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            ViewModel.SelectCommand.Execute(e.SelectedItem);
         }
     }
 }
