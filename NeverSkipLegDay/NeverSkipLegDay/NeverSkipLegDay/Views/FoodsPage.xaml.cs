308a0c20406c167b97ba769df869743d3812ad5c
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-08 10:52:27 +0100

    Added a stub for the FoodsPage

---
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Views/FoodsPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using NeverSkipLegDay.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NeverSkipLegDay.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FoodsPage : ContentPage
+    {
+        public FoodsPage(MealViewModel meal)
+        {
+            InitializeComponent();
+        }
+    }
+}
