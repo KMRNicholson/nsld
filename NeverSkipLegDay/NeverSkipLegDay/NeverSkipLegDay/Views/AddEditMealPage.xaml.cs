063abc4c34172649b92d0dc7523ccc47c8a52511
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-08 10:47:16 +0100

    AddEditMealPage created

---
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Views/AddEditMealPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using NeverSkipLegDay.ViewModels;
+using NeverSkipLegDay.Models.DAL;
+
+namespace NeverSkipLegDay.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddEditMealPage : ContentPage
+    {
+        public AddEditMealPageViewModel ViewModel
+        {
+            get { return BindingContext as AddEditMealPageViewModel; }
+            set { BindingContext = value; }
+        }
+        public AddEditMealPage(MealViewModel mealViewModel)
+        {
+            var mealDal = new MealDal(new SQLiteDB());
+            var pageService = new PageService();
+            ViewModel = new AddEditMealPageViewModel(mealViewModel, mealDal, pageService);
+            InitializeComponent();
+        }
+    }
+}
