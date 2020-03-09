936062b48bd006fc6c4c3ce25f1e898db9eb2be6
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-09 23:18:09 +0100

    Checking in the views for the records.

---
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Views/Records/RecordsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using NeverSkipLegDay.ViewModels;
+using NeverSkipLegDay.Models.DAL;
+
+namespace NeverSkipLegDay.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RecordsPage : ContentPage
+    {
+        public RecordsPageViewModel ViewModel
+        {
+            get { return BindingContext as RecordsPageViewModel; }
+            set { BindingContext = value; }
+        }
+        public RecordsPage()
+        {
+            var recordDal = new RecordDal(new SQLiteDB());
+            var pageService = new PageService();
+            ViewModel = new RecordsPageViewModel(recordDal, pageService);
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ViewModel.LoadDataCommand.Execute(null);
+            helpLabel.IsVisible = ViewModel.IsRecordsEmpty();
+        }
+    }
+}
