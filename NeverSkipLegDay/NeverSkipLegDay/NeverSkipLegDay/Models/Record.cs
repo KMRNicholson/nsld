e68a0204a27f225d9573a3c5f227a19f246e440c
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-09 18:42:53 +0100

    Model for record

---
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Models/Record.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace NeverSkipLegDay.Models
+{
+    public class Record
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public int? Reps { get; set; }
+        public int? Weight { get; set; }
+    }
+}
