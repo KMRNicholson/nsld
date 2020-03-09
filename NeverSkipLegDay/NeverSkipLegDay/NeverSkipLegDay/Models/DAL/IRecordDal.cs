c492a61b3d241bda1528a9f0a6d772ca3e41f4f2
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-09 18:54:10 +0100

    Record Dals created

---
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Models/DAL/IRecordDal.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace NeverSkipLegDay.Models.DAL
+{
+    public interface IRecordDal
+    {
+        List<Record> GetRecords();
+        Record GetRecord(int id);
+        int SaveRecord(Record model);
+        int DeleteRecord(Record model);
+    }
+}
---
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Models/DAL/RecordDal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using SQLite;
+
+namespace NeverSkipLegDay.Models.DAL
+{
+    public class RecordDal : IRecordDal
+    {
+        readonly SQLiteConnection _database;
+        public RecordDal(SQLiteDB db)
+        {
+            _database = db.GetConnection();
+            _database.CreateTable<Record>();
+        }
+
+        public List<Record> GetRecords()
+        {
+            return _database.Table<Record>().ToList();
+        }
+        public Record GetRecord(int id)
+        {
+            return _database.Table<Record>()
+                            .Where(i => i.Id == id)
+                            .FirstOrDefault();
+        }
+
+        public int SaveRecord(Record model)
+        {
+            if (model.Id != 0)
+            {
+                return _database.Update(model);
+            }
+            else
+            {
+                return _database.Insert(model);
+            }
+        }
+
+        public int DeleteRecord(Record model)
+        {
+            return _database.Delete(model);
+        }
+    }
+}
