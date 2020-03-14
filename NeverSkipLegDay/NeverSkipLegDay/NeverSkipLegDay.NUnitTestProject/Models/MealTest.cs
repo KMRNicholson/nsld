40a3f77965e41aeabddffa4d0b0f09a3caecb2ca
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-14 19:12:09 +0100

    Adding some functionality to get the totals from the foods in the meal.

---
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay.NUnitTestProject/Models/MealTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using NeverSkipLegDay.Models.DAL;
+using NeverSkipLegDay.ViewModels;
+using NeverSkipLegDay.Models;
+using System.Collections.Generic;
+using NeverSkipLegDay.NUnitTestProject.Database;
+using System.Threading.Tasks;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace NeverSkipLegDay.NUnitTestProject.ViewModels
+{
+    [TestFixture]
+    public class MealTest
+    {
+        [SetUp]
+        public void Constructor()
+        {
+
+        }
+
+        [Test]
+        public void EditExerciseConstructorTest()
+        {
+
+        }
+    }
+}
--- NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Models/Meal.cs
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Models/Meal.cs
@@ -1,4 +1,7 @@
-﻿using SQLite;
+﻿using NeverSkipLegDay.Models.DAL;
+using SQLite;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NeverSkipLegDay.Models
 {
@@ -8,5 +11,19 @@ namespace NeverSkipLegDay.Models
         public int Id { get; set; }
         [MaxLength(50)]
         public string Name { get; set; }
+
+        public Dictionary<string, int> GetTotals()
+        {
+            Dictionary<string, int> totals = new Dictionary<string, int>();
+
+            List<Food> foods = new FoodDal(new SQLiteDB()).GetFoodsByMealId(this.Id);
+
+            totals.Add("Fat", foods.Select(x => x.Fat).Sum().Value);
+            totals.Add("Prot", foods.Select(x => x.Prot).Sum().Value);
+            totals.Add("Carb", foods.Select(x => x.Carb).Sum().Value);
+            totals.Add("Cal", foods.Select(x => x.Cal).Sum().Value);
+
+            return totals;
+        }
     }
 }
