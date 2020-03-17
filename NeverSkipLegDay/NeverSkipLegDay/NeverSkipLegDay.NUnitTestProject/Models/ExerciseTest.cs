28d8389e9aec6d7b21e37da3ee400fed2f537170
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-17 20:40:55 +0100

    Added the sets and reps to the exercise view model

--- NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay.NUnitTestProject/Models/ExerciseTest.cs
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay.NUnitTestProject/Models/ExerciseTest.cs
@@ -1,12 +1,8 @@
 ﻿using NUnit.Framework;
-using NeverSkipLegDay.Models.DAL;
-using NeverSkipLegDay.ViewModels;
 using NeverSkipLegDay.Models;
 using System.Collections.Generic;
 using NeverSkipLegDay.NUnitTestProject.Database;
-using System.Threading.Tasks;
 using System.Linq;
-using Xamarin.Forms;
 
 namespace NeverSkipLegDay.NUnitTestProject.ViewModels
 {
