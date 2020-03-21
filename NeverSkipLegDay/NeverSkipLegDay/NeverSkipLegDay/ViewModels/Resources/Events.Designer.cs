1d1301502d6f69f3270ba9fb81c1d6345c917cac
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-21 18:51:21 +0100

    fixing namespace for the resources :D

--- NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/ViewModels/Resources/Events.Designer.cs
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/ViewModels/Resources/Events.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace NeverSkipLegDay.ViewModels.Resources {
+namespace NeverSkipLegDay.ViewModels {
     using System;
     
     
