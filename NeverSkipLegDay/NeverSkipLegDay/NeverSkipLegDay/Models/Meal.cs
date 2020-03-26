3fab67225841f7648ca485b64e5409a153e48927
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-26 19:17:34 +0100

    Meal model cleaned up.

--- NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Models/Meal.cs
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Models/Meal.cs
@@ -1,19 +1,32 @@
-﻿using NeverSkipLegDay.Models.DAL;
-using SQLite;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 
+using SQLite;
+
+using NeverSkipLegDay.Models.DAL;
+
 namespace NeverSkipLegDay.Models
 {
+    /*
+     * Class which defines the behavior and properties of the Meal model, and entity in the database.
+     */
     public class Meal
     {
+        #region attributes
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         [MaxLength(50)]
         public string Name { get; set; }
+        #endregion
 
+        #region public methods
+        // Method for collecting the total macronutrients and calories for the meal using the foodDal and the meal id.
         public Dictionary<string, int> GetMealTotals(IFoodDal foodDal)
         {
+            if (foodDal == null)
+                throw new ArgumentNullException(nameof(foodDal));
+
             Dictionary<string, int> totals = new Dictionary<string, int>();
 
             List<Food> foods = foodDal.GetFoodsByMealId(this.Id);
@@ -25,5 +38,6 @@ namespace NeverSkipLegDay.Models
 
             return totals;
         }
+        #endregion
     }
 }
