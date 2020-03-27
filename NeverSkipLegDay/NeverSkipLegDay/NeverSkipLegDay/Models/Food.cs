67b80c349451ae574227b10f526c9ce937bb98cd
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-27 10:01:50 +0100

    Modified the food model to use decimals instead of int? for macronutrients

--- NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Models/Food.cs
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/Models/Food.cs
@@ -4,16 +4,16 @@ namespace NeverSkipLegDay.Models
 {
     public class Food
     {
+        #region attributes
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public int MealId { get; set; }
         [MaxLength(50)]
         public string Name { get; set; }
-
-        public int? Fat { get; set; }
-        public int? Prot { get; set; }
-        public int? Carb { get; set; }
-        public int? Cal { get; set; }
-
+        public decimal Fat { get; set; }
+        public decimal Prot { get; set; }
+        public decimal Carb { get; set; }
+        public decimal Cal { get; set; }
+        #endregion
     }
 }
