6b41213d6597af0f847e11149b19d516daaca9fb
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-27 16:13:23 +0100

    Fixing warnings

--- NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/ViewModels/PageService.cs
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/ViewModels/PageService.cs
@@ -11,22 +11,22 @@ namespace NeverSkipLegDay.ViewModels
         
         public async Task<bool> DisplayAlert(string title, string message, string ok, string cancel)
         {
-            return await MainPage.DisplayAlert(title, message, ok, cancel);
+            return await MainPage.DisplayAlert(title, message, ok, cancel).ConfigureAwait(false);
         }
 
         public async Task DisplayAlert(string title, string message, string ok)
         {
-            await MainPage.DisplayAlert(title, message, ok);
+            await MainPage.DisplayAlert(title, message, ok).ConfigureAwait(false);
         }
 
         public async Task<Page> PopAsync()
         {
-            return await MainPage.Navigation.PopAsync();
+            return await MainPage.Navigation.PopAsync().ConfigureAwait(false);
         }
 
         public async Task PushAsync(Page page)
         {
-            await MainPage.Navigation.PushAsync(page);
+            await MainPage.Navigation.PushAsync(page).ConfigureAwait(false);
         }
         private Page MainPage
         {
