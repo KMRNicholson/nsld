33e6837b54c4570929eceb0603e4e303aad7ccb9
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-27 01:37:35 +0100

    AddEditMealPageViewModel reviewed.

--- NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/ViewModels/Nutrition/AddEditMealPageViewModel.cs
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/ViewModels/Nutrition/AddEditMealPageViewModel.cs
@@ -1,25 +1,37 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Windows.Input;
+
+using Xamarin.Forms;
+
 using NeverSkipLegDay.Models;
 using NeverSkipLegDay.Models.DAL;
-using Xamarin.Forms;
 
 namespace NeverSkipLegDay.ViewModels
 {
     public class AddEditMealPageViewModel : BaseViewModel
     {
-        private IMealDal _mealDal;
-        private IPageService _pageService;
+        #region private properties
+        private readonly IMealDal _mealDal;
+        private readonly IPageService _pageService;
+        #endregion
 
+        #region public properties
+        public string PageTitle { get; private set; }
         public Meal Meal { get; private set; }
+        #endregion
 
+        #region commands
         public ICommand SaveCommand { get; private set; }
+        #endregion
 
+        #region constructor
         public AddEditMealPageViewModel(MealViewModel meal, IMealDal mealDal, IPageService pageService)
         {
             if (meal == null)
                 throw new ArgumentNullException(nameof(meal));
+
+            PageTitle = "MEAL";
 
             _mealDal = mealDal;
             _pageService = pageService;
@@ -32,18 +44,21 @@ namespace NeverSkipLegDay.ViewModels
                 Name = meal.Name
             };
         }
+        #endregion
 
+        #region public methods
         public async Task Save()
         {
             if (string.IsNullOrWhiteSpace(Meal.Name))
             {
-                await _pageService.DisplayAlert("Error", "Please enter a name.", "OK");
+                await _pageService.DisplayAlert(DisplayAlerts.Error, DisplayAlerts.NullNameError, DisplayAlerts.Ok).ConfigureAwait(false);
                 return;
             }
 
             _mealDal.SaveMeal(Meal);
             MessagingCenter.Send(this, Events.MealSaved, Meal);
-            await _pageService.PopAsync();
+            await _pageService.PopAsync().ConfigureAwait(false);
         }
+        #endregion
     }
 }
