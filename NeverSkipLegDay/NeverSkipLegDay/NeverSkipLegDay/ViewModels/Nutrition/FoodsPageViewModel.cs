7b10288be24315ed11d5e478e4220ccbf0ae3edf
kmrnicholson <kohdy.nicholson@gmail.com>
2020-03-27 15:15:03 +0100

    FoodsPageViewModel is reviewed.

--- NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/ViewModels/Nutrition/FoodsPageViewModel.cs
+++ NeverSkipLegDay/NeverSkipLegDay/NeverSkipLegDay/ViewModels/Nutrition/FoodsPageViewModel.cs
@@ -1,22 +1,38 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Globalization;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
+
+using Xamarin.Forms;
+
 using NeverSkipLegDay.Models;
 using NeverSkipLegDay.Models.DAL;
 using NeverSkipLegDay.Views;
-using Xamarin.Forms;
 
 namespace NeverSkipLegDay.ViewModels
 {
+    /*
+     * The ViewModel for the FoodsPage.xaml.cs.
+     * Contains all methods for retrieving information from the model
+     * and displaying this information to the view.
+     */
     public class FoodsPageViewModel : BaseViewModel
     {
-        private IFoodDal _foodDal;
-        private IPageService _pageService;
-
-        private bool _isDataLoaded;
-
+        #region private properties
         private MealViewModel _meal;
+        private readonly IFoodDal _foodDal;
+        private readonly IPageService _pageService;
+        private bool _isDataLoaded;
+        private bool _showHelpLabel;
+        #endregion
+
+        #region public properties
+        public string PageTitle { get; private set; }
+        public string ButtonText { get; private set; }
+        public ObservableCollection<FoodViewModel> Foods { get; private set; }
+            = new ObservableCollection<FoodViewModel>();
         public MealViewModel Meal
         {
             get { return _meal; }
@@ -26,24 +42,32 @@ namespace NeverSkipLegDay.ViewModels
                 OnPropertyChanged(nameof(Meal));
             }
         }
-
-        public string AddButtonText
+        public bool ShowHelpLabel
         {
-            get { return "Add Food"; }
+            get { return _showHelpLabel; }
+            set
+            {
+                SetValue(ref _showHelpLabel, value);
+                OnPropertyChanged(nameof(ShowHelpLabel));
+            }
         }
+        #endregion
 
-        public ObservableCollection<FoodViewModel> Foods { get; private set; }
-            = new ObservableCollection<FoodViewModel>();
-
+        #region commands
         public ICommand LoadDataCommand { get; set; }
         public ICommand AddCommand { get; set; }
         public ICommand EditCommand { get; set; }
         public ICommand DeleteCommand { get; set; }
+        #endregion
 
+        #region constructor
         public FoodsPageViewModel(MealViewModel meal, IFoodDal foodDal, IPageService pageService)
         {
             MessagingCenter.Subscribe<AddEditFoodPageViewModel, Food>
                 (this, Events.FoodSaved, OnFoodSaved);
+
+            PageTitle = "FOODS";
+            ButtonText = "Add Food";
 
             _foodDal = foodDal;
             _pageService = pageService;
@@ -51,11 +75,53 @@ namespace NeverSkipLegDay.ViewModels
             Meal = meal;
 
             LoadDataCommand = new Command(() => LoadData());
-            AddCommand = new Command(async () => await AddFood());
-            EditCommand = new Command<FoodViewModel>(async food => await EditFood(food));
-            DeleteCommand = new Command<FoodViewModel>(async food => await DeleteFood(food));
+            AddCommand = new Command(async () => await AddFood().ConfigureAwait(false));
+            EditCommand = new Command<FoodViewModel>(async food => await EditFood(food).ConfigureAwait(false));
+            DeleteCommand = new Command<FoodViewModel>(async food => await DeleteFood(food).ConfigureAwait(false));
+        }
+        #endregion
+
+        #region public methods
+        // Asynchronous task for deleting food from the database and removing it from the list.
+        // params: FoodViewModel - required for removing the viewmodel from the list, and to delete the record in the database.
+        public async Task DeleteFood(FoodViewModel food)
+        {
+            if (food == null) return;
+
+            string warningMessage = string.Format(new CultureInfo("en-US"), DisplayAlerts.DeleteWarning, food.Name);
+
+            if (await _pageService.DisplayAlert(DisplayAlerts.Warning, warningMessage, DisplayAlerts.Yes, DisplayAlerts.No).ConfigureAwait(false))
+            {
+                var foodModel = _foodDal.GetFood(food.Id);
+                Foods.Remove(food);
+                _foodDal.DeleteFood(foodModel);
+            }
+
+            ShowHelpLabel = IsFoodsEmpty();
+
+            SetTotals();
+        }
+        #endregion
+
+        #region private methods
+        // Method which returns a list of foods associated to the selected meal by using the exercise id.
+        private void LoadData()
+        {
+            if (_isDataLoaded) return;
+
+            _isDataLoaded = true;
+            var foods = _foodDal.GetFoodsByMealId(Meal.Id);
+            foreach (var food in foods)
+            {
+                Foods.Add(new FoodViewModel(food));
+            }
+
+            ShowHelpLabel = IsFoodsEmpty();
+
+            SetTotals();
         }
 
+        // Method which saves a food to the database. Triggered by the MessageCenter event.
         private void OnFoodSaved(AddEditFoodPageViewModel source, Food food)
         {
             FoodViewModel foodInList = Foods.Where(e => e.Id == food.Id).ToList().FirstOrDefault();
@@ -76,49 +142,27 @@ namespace NeverSkipLegDay.ViewModels
             }
         }
 
-        public void LoadData()
-        {
-            if (_isDataLoaded) return;
-
-            _isDataLoaded = true;
-            var foods = _foodDal.GetFoodsByMealId(Meal.Id);
-            foreach (var food in foods)
-            {
-                Foods.Add(new FoodViewModel(food));
-            }
-        }
-
+        // Method which adds and saves a new food to the list and database.
         private async Task AddFood()
         {
-            await _pageService.PushAsync(new AddEditFoodPage(new FoodViewModel() { MealId = Meal.Id }));
+            await _pageService.PushAsync(new AddEditFoodPage(new FoodViewModel() { MealId = Meal.Id })).ConfigureAwait(false);
         }
 
+        // Method which edits and saves food to the list and database.
         private async Task EditFood(FoodViewModel food)
         {
             if (food == null) return;
 
-            await _pageService.PushAsync(new AddEditFoodPage(food));
+            await _pageService.PushAsync(new AddEditFoodPage(food)).ConfigureAwait(false);
         }
 
-        public async Task DeleteFood(FoodViewModel food)
-        {
-            if (food == null) return;
-
-            if (await _pageService.DisplayAlert("Warning", $"Are you sure you want to delete {food.Name}?", "Yes", "No"))
-            {
-                var foodModel = _foodDal.GetFood(food.Id);
-                Foods.Remove(food);
-                _foodDal.DeleteFood(foodModel);
-            }
-
-            SetTotals();
-        }
-
+        // Method which checks to see if the food list is empty, in order to display the help label in the view.
         public bool IsFoodsEmpty()
         {
             return Foods.Count == 0 ? true : false;
         }
 
+        // Method which gets a new meal model to update the macros and cals to display in the view.
         public void SetTotals()
         {
             //THIS SHOULD NOT ACTUALLY CALL THE DAL. UPDATE THE CURRENT MODEL ON THE FLY
@@ -127,5 +171,6 @@ namespace NeverSkipLegDay.ViewModels
 
             Meal = meal != null ? new MealViewModel(meal) : Meal;
         }
+        #endregion
     }
 }
